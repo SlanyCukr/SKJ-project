3fb3866ef5bb68bb48f409db72641c2192c652a1
Marek Hovadík <hovadik@atlasgroup.cz>
2021-03-22 09:05:10 +0100

    Extracting basic information from article Crawling starts on stalo-se timeline

--- scraper/scraper_novinky.py
+++ scraper/scraper_novinky.py
@@ -1,17 +1,72 @@
+from datetime import datetime
+import json
 import mechanicalsoup
+
+from classes.Article import Article
 
 BASE_URL = "https://novinky.cz/stalo-se"
 
 
+def retrieve_paragraphs(page) -> []:
+    paragraphs = page.select('div[data-dot-data=\'{"component":"article-content"}\'] p')
+
+    text_list = []
+    for paragraph in paragraphs:
+        text_list.append(paragraph.text)
+    return text_list
+
+
+def retrieve_authors(page) -> []:
+    authors_select = page.select('div[class="g_gz"] a[class="d_aZ"]')
+
+    authors = []
+    for link in authors_select:
+        authors.append(link.text)
+
+    return authors
+
+
+def find_category(first_script_data) -> str:
+    item_list_element = first_script_data["itemListElement"]
+    for list_element in item_list_element:
+        if list_element["position"] == 2:
+            return list_element["name"]
+
+
+def extract_article(browser: mechanicalsoup.StatefulBrowser, url: str) -> Article:
+    browser.open(url)
+    page = browser.get_current_page()
+
+    script_data = page.select('script[type="application/ld+json"]')
+    first_script_data = json.loads(script_data[1].text)
+    second_script_data = json.loads(script_data[0].text)
+
+    header = second_script_data["headline"]
+    description = second_script_data["description"]
+    category = find_category(first_script_data)
+    published_at = datetime.strptime(second_script_data["datePublished"], '%Y-%m-%dT%H:%M:%S.%fZ')
+    modified_at = datetime.strptime(second_script_data["dateModified"], '%Y-%m-%dT%H:%M:%S.%fZ')
+    authors = retrieve_authors(page)
+    paragraphs = retrieve_paragraphs(page)
+
+    #return Article(header=header, description=description, category=category, published_at=published_at, modified_at=modified_at, authors=authors, paragraphs=paragraphs)
+
+
 if __name__ == '__main__':
     browser = mechanicalsoup.StatefulBrowser()
-    browser.open(BASE_URL   )
+    browser.open(BASE_URL)
 
     page = browser.get_current_page()
 
+    # we will crawl these sites
+    articles = []
     stalo_se_articles = page.select('div[data-dot="stalo_se"] a')
     for article in stalo_se_articles:
-        print(article['href'])
+        articles.append(extract_article(browser, article['href']))
+
+
+
+
 
 
 
