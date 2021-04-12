c8944c1355333c891fa036d74c1c93639578b79e
Marek Hovadík <hovadik@atlasgroup.cz>
2021-04-12 12:17:10 +0200

    Fixed logical error in GraphQL SQL

--- graphql_backend/db_utils.py
+++ graphql_backend/db_utils.py
@@ -8,7 +8,7 @@ db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind
 def get_comments():
     with engine.connect() as connection:
         today = connection.execute("SELECT COUNT(*) FROM comment WHERE date(created_on) > date('now', '-1 days')")
-        day_old = connection.execute("SELECT COUNT(*) FROM comment WHERE date(created_on) BETWEEN date('now', '-2 days') AND date('now', '-1 days')")
+        day_old = connection.execute("SELECT COUNT(*) FROM comment WHERE date(created_on) = date('now', '-1 days')")
         all = connection.execute("SELECT COUNT(*) FROM comment")
 
         return list(today)[0][0], list(day_old)[0][0], list(all)[0][0]
@@ -17,7 +17,7 @@ def get_comments():
 def get_articles():
     with engine.connect() as connection:
         today = connection.execute("SELECT COUNT(*) FROM article WHERE date(created_on) > date('now', '-1 days')")
-        day_old = connection.execute("SELECT COUNT(*) FROM article WHERE date(created_on) BETWEEN date('now', '-2 days') AND date('now', '-1 days')")
+        day_old = connection.execute("SELECT COUNT(*) FROM article WHERE date(created_on) = date('now', '-1 days')")
         all = connection.execute("SELECT COUNT(*) FROM article")
 
         return list(today)[0][0], list(day_old)[0][0], list(all)[0][0]
