49e0a6ecf6f687fdad6ba5b3d9e6d73f99b7f64c
Marek Hovadík <hovadik@atlasgroup.cz>
2021-04-12 08:54:14 +0200

    Abandoning ngrok - can't access local GraphQL server

--- install.sh
+++ install.sh
@@ -3,8 +3,3 @@ pip3 install -r requirements.txt
 
 cd frontend
 npm install
-
-cd ..
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
-unzip ngrok-stable-linux-arm.zip
-rm ngrok-stable-linux-arm.zip
--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 screen -S scraper -dm python3 -m scraper.scraper_novinky
 screen -S graphql_server -dm python3 -m graphql_backend.server
-screen -S ngrok -dm ./ngrok http -region eu 3000
 
 cd frontend
 screen -S react -dm npm start
