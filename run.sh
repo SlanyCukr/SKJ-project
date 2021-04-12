187b6b12a7f5e167b96fa8d0050ec9fe01fdb594
Marek Hovadík <hovadik@atlasgroup.cz>
2021-04-12 07:00:31 +0200

    Scripts altered for running this on PI

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 screen -S scraper -dm python3 -m scraper.scraper_novinky
 screen -S graphql_server -dm python3 -m graphql_backend.server
+screen -S ngrok -dm ./ngrok http -region eu 3000
+
+cd frontend
+screen -S react -dm npm start
--- stop.sh
+++ stop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 screen -X -S scraper quit
 screen -X -S graphql_server quit
+screen -X -S ngrok quit
+screen -X -S react quit
