9c56156fc1804b151bb6de18565e004e1ecc9ffa
Marek Hovadík <hovadik@atlasgroup.cz>
2021-04-12 07:25:54 +0200

    Port change from 5000 to 5001 on graphql server

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: 'http://localhost:5001/graphql',
   cache: new InMemoryCache()
 });
 
