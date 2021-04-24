71fba5cd20e54fef73c8f390c8821c1cb18fe98b
Marek Hovadík <hovadik@atlasgroup.cz>
2021-04-24 06:29:49 +0200

    More options to chromedriver - trying to prevent timeout exception

--- scraper/scraper_novinky.py
+++ scraper/scraper_novinky.py
@@ -19,10 +19,14 @@ BASE_URL = "https://novinky.cz/stalo-se"
 
 def get_browser():
     chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('start-maximized')
+    chrome_options.add_argument('enable-automation')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-infobars')
+    chrome_options.add_argument('--disable-browser-side-navigation')
     try:
         if os.uname().nodename == 'raspberrypi':
             return webdriver.Chrome(options=chrome_options)
