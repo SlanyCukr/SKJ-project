c5fb39e5be318e346f5df3746335a22cf40f6b75
Marek Hovadík <hovadik@atlasgroup.cz>
2021-04-24 07:21:29 +0200

    No need to log graphql_server

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 screen -S scraper -dmL python3 -m scraper.scraper_novinky
-screen -S graphql_server -dmL python3 -m graphql_backend.server
+screen -S graphql_server -dm python3 -m graphql_backend.server
 
 cd frontend
 screen -S react -dm npm start
